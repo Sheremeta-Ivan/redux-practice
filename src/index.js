3c49f1c2743215149cac2b5d9b68eb4d169719ed
Sheremeta Ivan <sheremetavanya2004@gmail.com>
2023-09-07 13:56:55 +0300

    create store/action/bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, bindActionCreators } from "redux";
+import reducer from "./reducer";
+import * as actions from "./actions";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const store = createStore(reducer);
+const { dispatch, subscribe, getState } = store;
+
+const update = () => {
+  document.getElementById("counter").textContent = getState().value;
+};
+
+subscribe(update);
+
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+
+document.getElementById("inc").addEventListener("click", inc);
+
+document.getElementById("dec").addEventListener("click", dec);
+
+document.getElementById("rnd").addEventListener("click", () => {
+  const value = Math.floor(Math.random() * 10);
+  rnd(value);
+});
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <></>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
